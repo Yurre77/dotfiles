198d574794868a4be49a09a5f827f635194ebcb3
Yurre <yurrr.schram@gmail.com>
2025-12-31 20:42:43 +0100

    minor updates

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -6,10 +6,12 @@ dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
 swaync &
 
-swaybg -i ~/Pictures/wallpapers/kanagawa.png &
+swaybg -i ~/.config/mango/background.png &
 
 waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css &
 
 wl-clip-persist --clipboard regular &
 
 wl-paste --watch cliphist store &
+
+/usr/lib/xfce-polkit/xfce-polkit &
